00faf5b762663935fea2640ae5f70ee00ec83b06
Will Figueiredo <will@wfigueiredo.com>
2018-10-04 03:03:16 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,6 +29,9 @@ def monopoly_with_third_tier
 		:railroads => {
 			:pieces	=> 4,
 			:names => {
+				:reading_railroad => {},
+				:pennsylvania_railroad => {},
+				:b_and_o_railroad => {},
 				:shortline => {}
 			},
 			:rent_in_dollars => {
